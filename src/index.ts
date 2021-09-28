588df9db6c605cb3090ec8077c9420a93d5dcc03
luke-h1 <luke.howsam@yahoo.com>
2021-09-28 07:41:38 +0100

    (fix): throw type error if not string

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const keebab = (string: string) => {
   if (string !== 'string') {
-    throw new TypeError(`Expected string, received}!`)
+    throw new TypeError(`Expected string, received ${string}!`);
   }
   const result = string.split(' ').join('-');
   return result;
